7cfb48e7c557a7f9aa486287497270745c4de378
devildev98 <debarghya.datta89@gmail.com>
2021-12-12 16:31:42 +0530

    Tabbed Initial Commit

--- config.mk
+++ config.mk
@@ -7,12 +7,12 @@ VERSION = 0.6
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
+FREETYPEINC = /usr/local/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 
